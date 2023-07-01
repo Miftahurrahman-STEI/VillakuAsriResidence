f179d0f79893b3e51d6915257bff2a17d67906a4
gnta <pgenta17@gmail.com>
2023-07-01 09:00:07 +0700

    [refac] code

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,27 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'Koskosan',
-      component: () => import('../views/KoskosanView.vue')
+      path: "/",
+      name: "Root",
+      component: () => import("../views/KoskosanView.vue"),
     },
     {
-      path: '/koskosan',
-      name: 'Koskosan',
-      component: () => import('../views/KoskosanView.vue')
+      path: "/koskosan",
+      name: "Koskosan",
+      component: () => import("../views/KoskosanView.vue"),
     },
     {
-      path: '/bungallow',
-      name: 'bungallow',
+      path: "/bungallow",
+      name: "bungallow",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/BungallowView.vue')
-    }
-  ]
-})
+      component: () => import("../views/BungallowView.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
