d1a7fb2a615d12a50027234db2be53737e8b02da
Miftahurrahman-STEI <mippganz@gmail.com>
2023-07-02 17:59:42 +0700

    menambahkan maps & footer bungallow, sedikit perubahan file yg hilang di koskosan

--- src/views/KoskosanView.vue
+++ src/views/KoskosanView.vue
@@ -189,11 +189,269 @@ import { RouterLink, RouterView } from 'vue-router'
             </div>
           </div>
         </div>
+        <!-- carousel card ulasan -->
+        <div class="container-card">
+          <h2>Ulasan</h2>
+            <ul class="cards">
+              <li class="card">
+                <div>
+                  <div class="card-content">
+                      <div class="ulasan-card">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                      </div>
+                  </div>
+                    <div class="ulasan-deskripsi">
+                      <p>Tempatnya strategis dan nyaman, parkiran luas, CCTV, dan penjagaan 24jam</p>
+                    </div>
+                    <div class="ulasan-user">
+                      <h3>Abang Kumis</h3>
+                      <p>Review Google</p>
+                    </div>
+                </div>
+              </li>
+              <li class="card">
+                <div>
+                  <div class="card-content">
+                      <div class="ulasan-card">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                      </div>
+                  </div>
+                    <div class="ulasan-deskripsi">
+                      <p>Fasilitas nyaman, keamanan 24jam, recommended bgt buat pekerja, mahasiswa, akses deket kemana mana👍</p>
+                    </div>
+                    <div class="ulasan-user">
+                      <h3>Tegar Maulana</h3>
+                      <p>Review Google</p>
+                    </div>
+                </div>
+              </li>
+              <li class="card">
+                <div>
+                  <div class="card-content">
+                      <div class="ulasan-card">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                      </div>
+                  </div>
+                    <div class="ulasan-deskripsi">
+                      <p>Bersih</p>
+                    </div>
+                    <div class="ulasan-user">
+                      <h3>Daud Setiawan</h3>
+                      <p>Review Google</p>
+                    </div>
+                </div>
+              </li>
+              <li class="card">
+                <div>
+                  <div class="card-content">
+                      <div class="ulasan-card">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                      </div>
+                  </div>
+                    <div class="ulasan-deskripsi">
+                      <p></p>
+                    </div>
+                    <div class="ulasan-user">
+                      <h3>azaria zalsa</h3>
+                      <p>Review Google</p>
+                    </div>
+                </div>
+              </li>
+              <li class="card">
+                <div>
+                  <div class="card-content">
+                      <div class="ulasan-card">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                      </div>
+                  </div>
+                    <div class="ulasan-deskripsi">
+                      <p></p>
+                    </div>
+                    <div class="ulasan-user">
+                      <h3>wi deanto él sarosa</h3>
+                      <p>Review Google</p>
+                    </div>
+                </div>
+              </li>
+              <li class="card">
+                <div>
+                  <div class="card-content">
+                      <div class="ulasan-card">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                      </div>
+                  </div>
+                    <div class="ulasan-deskripsi">
+                      <p></p>
+                    </div>
+                    <div class="ulasan-user">
+                      <h3>Sak Bahagia Mu</h3>
+                      <p>Review Google</p>
+                    </div>
+                </div>
+              </li>
+              <li class="card">
+                <div>
+                  <div class="card-content">
+                      <div class="ulasan-card">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                      </div>
+                  </div>
+                    <div class="ulasan-deskripsi">
+                      <p></p>
+                    </div>
+                    <div class="ulasan-user">
+                      <h3>Sak Bahagia Mu</h3>
+                      <p>Review Google</p>
+                    </div>
+                </div>
+              </li>
+              <li class="card">
+                <div>
+                  <div class="card-content">
+                      <div class="ulasan-card">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                      </div>
+                  </div>
+                    <div class="ulasan-deskripsi">
+                      <p></p>
+                    </div>
+                    <div class="ulasan-user">
+                      <h3>Vania Tirsa Azalia</h3>
+                      <p>Review Google</p>
+                    </div>
+                </div>
+              </li>
+              <li class="card">
+                <div>
+                  <div class="card-content">
+                      <div class="ulasan-card">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                      </div>
+                  </div>
+                    <div class="ulasan-deskripsi">
+                      <p></p>
+                    </div>
+                    <div class="ulasan-user">
+                      <h3>Sutiaman Ujang</h3>
+                      <p>Review Google</p>
+                    </div>
+                </div>
+              </li>
+              <li class="card">
+                <div>
+                  <div class="card-content">
+                      <div class="ulasan-card">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                      </div>
+                  </div>
+                    <div class="ulasan-deskripsi">
+                      <p></p>
+                    </div>
+                    <div class="ulasan-user">
+                      <h3>Henry Ardika Fatih</h3>
+                      <p>Review Google</p>
+                    </div>
+                </div>
+              </li>
+              <li class="card">
+                <div>
+                  <div class="card-content">
+                      <div class="ulasan-card">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                      </div>
+                  </div>
+                    <div class="ulasan-deskripsi">
+                      <p></p>
+                    </div>
+                    <div class="ulasan-user">
+                      <h3>Suhada Edward</h3>
+                      <p>Review Google</p>
+                    </div>
+                </div>
+              </li>
+              <li class="card">
+                <div>
+                  <div class="card-content">
+                      <div class="ulasan-card">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                        <img src="../assets/stars.png" alt="stars">
+                      </div>
+                  </div>
+                    <div class="ulasan-deskripsi">
+                      <p></p>
+                    </div>
+                    <div class="ulasan-user">
+                      <h3>Sofy Anggraheni</h3>
+                      <p>Review Google</p>
+                    </div>
+                </div>
+              </li>
+            </ul>
+        </div>
+        <div class="peta">
+          <div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1981.8587399738026!2d106.8458442!3d-6.5573046!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69c798ee4ffde9%3A0x26ee544786731e91!2sVillaku%20asri%20residence!5e0!3m2!1sid!2sid!4v1688021925607!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+          <div class="footer_location">
+              <h1 id="title_location">Lokasi</h1>
+              <p>Ds cipambuan rt 01/01, Cipambuan, Kec. Babakan Madang,<br/>Kabupaten Bogor, Jawa Barat 16811</p>
+              <a href="https://wa.me/+6281280499596" class="button_contact"><img src="../assets/brand-whatsapp.png" alt="whatsapp"/>Hubungi Kami Sekarang</a>
+            </div>
+        </div>
     </div>
   </main>
 </template>
 
 <style scoped>
+::-webkit-scrollbar{
+  overflow:hidden;
+}
+
  *{
     margin: 0;
     padding: 0;
@@ -299,6 +557,7 @@ import { RouterLink, RouterView } from 'vue-router'
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+        overflow-x: hidden;
   }
   nav{
     width: 100%;
@@ -502,5 +761,204 @@ import { RouterLink, RouterView } from 'vue-router'
     margin-top: 2rem;
     margin-bottom: 3rem;
   }
+
+  .ruangan{
+    margin-bottom: 4.5rem;
+  }
+
+  .peta{
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    margin-top: 10rem;
+    text-align: center;
+  }
+  .peta .footer_location #title_location{
+    font-size: 45px;
+    text-align: center;
+    font-weight: bold;
+    color:#000;
+    padding-left: 0;
+  }
+  .footer_location p{
+    color:#000;
+    font-size: 16px;
+    margin-bottom: 2rem;
+  }
+  .peta .footer_location .button_contact{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-decoration: none;
+    padding: 20px 0;
+    border-radius: 21px;
+    background-color: #FBEB31;
+    color:#000;
+    gap: 10px;
+    cursor: pointer;
+  }
+  .peta .footer_location .button_contact img{
+    width: 20px;
+    height: 20px;
+  }
+
+    /* ulasan carousel */
+  .container-card {
+  max-width: 1400px;
+  padding: 0 15px;
+  margin: 0 auto;
+}
+.container-card h2{
+  font-size: 30px;
+  color: #000;
+  font-weight: bold;
+  margin-left: 4rem;
+}
+
+.cards {
+  display: flex;
+  padding: 25px 0px;
+  margin-left: 4rem;
+  list-style: none;
+  overflow-x: scroll;
+  scroll-snap-type: x mandatory;
+}
+
+.card {
+  display: flex;
+  flex-direction: column;
+  flex: 0 0 100%;
+  padding: 20px;
+  background: var(--white);
+  border-radius: 12px;
+  border: 2px solid #B3B3B3;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 15%);
+  scroll-snap-align: start;
+  transition: all 0.2s;
+}
+
+.card .card-content .ulasan-card{
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  gap: 7px;
+
+}
+.card .card-content .ulasan-card img{
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  width: 20px;
+  height: 20px;
+  margin-bottom: 10px;
+  object-fit: contain;
+}
+
+.card .ulasan-deskripsi {
+  margin-bottom: 10px;
+}
+.card .ulasan-user{
+  display:flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.card .ulasan-user h3{
+  font-size: 15px;
+  font-weight: bold;
+  font-style: italic;
+  border-right: 3px solid lightgray;
+  padding-right: 10px;
+  color: #000;
+}
+.card .ulasan-user p{
+  display:flex;
+  justify-content:left;
+  align-items:center;
+  font-style: italic;
+  color:#B8BABD;
+}
+
+.card .card-content img{
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+}
+
+.card:not(:last-child) {
+  margin-right: 10px;
+}
+
+.card .card-link-wrapper {
+  margin-top: auto;
+}
+
+.card .card-link {
+  display: inline-block;
+  text-decoration: none;
+  color: white;
+  background: var(--red);
+  padding: 6px 12px;
+  border-radius: 8px;
+  transition: background 0.2s;
+}
+
+.card:hover .card-link {
+  background: var(--darkred);
+}
+
+.cards::-webkit-scrollbar {
+  height: 12px;
+}
+
+.cards::-webkit-scrollbar-thumb,
+.cards::-webkit-scrollbar-track {
+  border-radius: 92px;
+}
+
+.cards::-webkit-scrollbar-thumb {
+  background: var(--darkred);
+}
+
+.cards::-webkit-scrollbar-track {
+  background: var(--thumb);
+}
+
+@media (min-width: 500px) {
+  .card {
+    flex-basis: calc(50% - 10px);
+  }
+
+  .card:not(:last-child) {
+    margin-right: 20px;
+  }
+}
+
+@media (min-width: 1100px) {
+  .card {
+    flex-basis: calc(25% - 30px);
+  }
+
+  .card:not(:last-child) {
+    margin-right: 40px;
+  }
+}
+
+
+/* FOOTER STYLES
+–––––––––––––––––––––––––––––––––––––––––––––––––– */
+.page-footer {
+  position: fixed;
+  right: 0;
+  bottom: 50px;
+  display: flex;
+  align-items: center;
+  padding: 5px;
+  z-index: 1;
+}
+
+.page-footer a {
+  display: flex;
+  margin-left: 4px;
+}
 }
 </style>
